4d25616a5667e1b0d006a19d360dfb295d8b7c1e
Stineliedicke <stineliedicke@hotmail.com>
2024-10-30 10:30:05 +0100

    javascript calender changer
    Added arrows to calender og javascript til calender changer

--- javascript/calender-changer.js
+++ javascript/calender-changer.js
@@ -2,4 +2,37 @@
 
 /*Image slider calender*/
 
+/*Vores elementer*/
+const calenderImage = document.getElementById("calenderImage");
+const arrowBack = document.getElementById("arrowBack");
+const arrowNext = document.getElementById("arrowNext");
+
+
+/*Billederne i et array*/
+const images = [
+    "assets/images/mobile/october-calender.svg",
+    "assets/images/mobile/november-calender.svg",
+    "assets/images/mobile/december-calender.svg"
+];
+
+let currentIndex = 0; //indeks 0 er vores første billede
+
+//den funktion der opdaterer billedet
+function updateImage (){
+    calenderImage.src = images[currentIndex];
+}
+
+
+//event listener på pilene
+
+arrowBack.addEventListener("click", () => {
+    currentIndex = (currentIndex === 0) ? images.length - 1 : currentIndex - 1;
+    updateImage();
+});
+
+arrowNext.addEventListener("click", () => {
+    currentIndex = (currentIndex === images.length - 1) ? 0 : currentIndex + 1;
+    updateImage();
+});
+
 
